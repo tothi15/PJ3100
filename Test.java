b8d559be1c2c3405a7a81586e29a177ff11f526b
Thinh <tothi15@student.westerdals.no>
2016-10-25 12:46:08 +0200

    testing

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test{
+    String name = "";
+    public Test(){
+        name = changeName("Thinh2");
+    }
+    
+    public void changeName(String name){
+        this.name = name;
+    }
+}
