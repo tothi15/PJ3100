7fab4baf801672d4405ee86e112b431f33ed0a5e
Thinh <tothi15@student.westerdals.no>
2016-10-25 13:10:43 +0200

    blabla

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test{
     String name = "";
     public Test(){
-        name = changeName("Thinh5");
+        name = changeName("Thinh6");
     }
     
     public void changeName(String name){
